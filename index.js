12d8e94789a5a15438011751a2a82b5ba536f642
VitorBonanca <80424109+VitorBonanca@users.noreply.github.com>
2021-07-12 14:00:50 -0300

    Banco de Dados Habilitado

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 (async () => {
-  const url = "mongodb://localhost:27017";
-  const dbName = "ocean_bancodados_2021-07-09";
+  const url = "mongodb+srv://admin:Yc3bmr45TISNE7dY@cluster0.krwr1.mongodb.net/ocean_db?retryWrites=true&w=majority";
+  const dbName = "ocean_db";
 
   console.info("Conectando ao banco de dados...");
 
-  // const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
   console.info("MongoDB conectado com sucesso!");
 
-  // const db = client.db(dbName);
+  const db = client.db(dbName);
 
   const app = express();
 
@@ -23,8 +23,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 
   const lista = ["Senhor dos Anéis", "Harry Potter"];
 
-  // const filmes = db.collection("Filmes");
-  const filmes = undefined;
+  const filmes = db.collection("filmes");
 
   // [GET] - Read All
   app.get("/filmes", async (req, res) => {
