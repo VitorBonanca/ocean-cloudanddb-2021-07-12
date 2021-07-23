89e6402da0acaf8fd2c3551f818d4cffd81378f3
VitorBonanca <80424109+VitorBonanca@users.noreply.github.com>
2021-07-23 12:58:11 -0300

    Update index.js
    Adicionando CORS

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+
+// CORS
+
+    app.all('/*', (req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+
+        res.header('Access-Control-Allow-Methods', '*');
+
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
+        );
+
+        next();
+    });
+
+
 const { MongoClient, ObjectId } = require("mongodb");
 
 (async () => {
